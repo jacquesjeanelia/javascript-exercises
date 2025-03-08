37b1d221732ccc17da2c6076edd53cc4f509d1d9
jacquesjeanelia <jacquesjeanelia@aucegypt.edu>
2025-03-08 16:21:06 +0200

    Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, occurrences) {
+    if (occurrences < 0) {
+        return 'ERROR';
+    }
+    let output = '';
+    for (let i = 0; i < occurrences; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
