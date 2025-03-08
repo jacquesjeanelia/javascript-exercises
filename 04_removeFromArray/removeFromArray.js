5a88128b83830926b2711d84c26a91dcfda48354
jacquesjeanelia <jacquesjeanelia@aucegypt.edu>
2025-03-08 16:39:06 +0200

    {commit_message}

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === args[i]) {
+                array.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
